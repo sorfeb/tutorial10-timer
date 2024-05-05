bcf0ad5ea56d2ff13de54b683d0d5854b6f1acfb
sorfeb <soros21febriano@gmail.com>
2024-05-05 23:59:43 +0700

    Experiment 1.3: Multiple Spawn and removing drop

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 ![alt text](images/image-1.png)
 
 The code runs the line `println!("Soros's computer: hey hey!");` first which is after the spawn spawner code block. This is because `executor.run()` line is placed below that print statement
+
+## Experiment 1.3: Multiple Spawn and removing drop
+![alt text](images/image-2.png)
+
+The tasks are spawned simultaneously, but the completion of tasks are done asynchronously, resulting the `Soros's computer: done[number]!` messages printed not printed as order.
+
+Other than that, the terminal is still running indefinitely because it is still waiting for tasks to be executed although it has been executed, this is caused because there is no `drop(spawner);`. 
--- src/main.rs
+++ src/main.rs
@@ -106,12 +106,24 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("Soros's computer: done!");
     });
+    spawner.spawn(async {
+        println!("Soros's computer howdy2!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Soros's computer: done2!");
+    });
+    spawner.spawn(async {
+        println!("Soros's computer howdy3!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Soros's computer: done3!");
+    });
 
 
     println!("Soros's computer: hey hey!");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
-    drop(spawner);
+    // drop(spawner);
 
     // Run the executor until the task queue is empty.
     // This will print "howdy!", pause, and then print "done!".
